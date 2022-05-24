0158d2912c1139585f70cdc8203685fa1e2ab8ec
musta <a20musma@student.his.se>
2022-05-24 15:32:11 +0200

    Jag har skapat knappen för internal web page. Med meny folder som innehåller mennu_main, I MainActivity.java Jag har lagt till komponenterna för det för inrternal sidan. I strings.xml Jag har lagt till string name

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener{
+
+
+
+    public void showInternalWebPage(){
+        // TODO: Add your code for showing internal web page here
+    }
 
     private final String JSON_FILE = "cars.json";
 
@@ -15,6 +24,31 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+
+        if (id == R.id.action_internal_web) {
+            Log.d("==>","Will display internal web page");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
